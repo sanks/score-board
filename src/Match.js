c7fa8775aef7a26bd71a820aeaedd26cdb76c43b
sanks <vadim.sanko@gmail.com>
2025-01-22 19:31:03 +0100

    Implement Match

--- src/Match.js
+++ src/Match.js
@@ -1,4 +1,41 @@
 class Match {
+
+    constructor(homeTeam, awayTeam) {
+        if (
+            !homeTeam 
+            || !awayTeam 
+            || typeof homeTeam !== "string" 
+            || typeof awayTeam !== "string"
+        ) {
+            throw new Error("Failed to create a match: please pass strings as homeTeam and awayTeam")
+        }
+
+        this.homeTeam = homeTeam
+        this.awayTeam = awayTeam
+        this.homeTeamScore = 0
+        this.awayTeamScore = 0
+        this.created = new Date()
+    }
+
+    setScore(homeTeamScore, awayTeamScore) {
+        if (
+            typeof homeTeamScore !== "number"
+            || typeof awayTeamScore !== "number"
+            || homeTeamScore < 0
+            || !Number.isInteger(homeTeamScore)
+            || awayTeamScore < 0
+            || !Number.isInteger(awayTeamScore)
+        ) {
+            throw new Error("Failed to set score: please pass positive integers for homeTeamScore and awayTeamScore")
+        }
+
+        this.homeTeamScore = homeTeamScore
+        this.awayTeamScore = awayTeamScore
+    }
+
+    get totalScore() {
+        return this.homeTeamScore + this.awayTeamScore
+    }
     
 }
 
